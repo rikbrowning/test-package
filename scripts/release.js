e22748ea655abe0d868abe4aa32137a345e85954
Richard Browning <rbrowning@nanthealth.com>
2021-08-03 16:15:47 -0400

    Trying to get changelog to generate.

--- scripts/release.js
+++ scripts/release.js
@@ -5,4 +5,5 @@ const shell = require('shelljs');
 shell.exec('git checkout main');
 shell.exec('git fetch');
 shell.exec(`npm version ${version} -m "release %s"`);
+shell.exec(`git push`);
 shell.exec(`git push origin v${version}`);
