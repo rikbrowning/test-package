4a1c32532a18b3c3a82b0f852b40a56091bdf43f
Richard Browning <rbrowning@nanthealth.com>
2021-08-03 15:48:54 -0400

    Adding release script

---
+++ scripts/release.js
@@ -0,0 +1,8 @@
+const version = process.argv[2];
+
+const shell = require('shelljs');
+
+shell.exec('git checkout main');
+shell.exec('git fetch');
+shell.exec(`npm version ${version} -m "release %s"`);
+shell.exec(`git push origin v${version}`);
