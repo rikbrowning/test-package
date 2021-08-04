eca7cf5b7118478b747db27578ad7e7164d6f7d4
Richard Browning <rbrowning@nanthealth.com>
2021-08-04 14:40:30 -0400

    feat(ci): removing release script and deps

--- scripts/release.js
+++
@@ -1,9 +0,0 @@
-const version = process.argv[2];
-
-const shell = require('shelljs');
-
-shell.exec('git checkout main');
-shell.exec('git fetch');
-shell.exec(`npm version ${version} -m "release %s"`);
-shell.exec(`git push`);
-shell.exec(`git push origin v${version}`);
